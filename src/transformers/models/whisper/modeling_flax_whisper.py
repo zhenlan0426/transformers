a4fb7455f772dedd2cf9a5a10b98971a85f7835e
will <zhenlanwang0426@gmail.com>
2023-08-07 15:32:16 -0400

    remove layerdrop

--- src/transformers/models/whisper/modeling_flax_whisper.py
+++ src/transformers/models/whisper/modeling_flax_whisper.py
@@ -426,11 +426,7 @@ class FlaxWhisperEncoderLayerCollection(nn.Module):
             if output_hidden_states:
                 all_hidden_states = all_hidden_states + (hidden_states,)
             # add LayerDrop (see https://arxiv.org/abs/1909.11556 for description)
-            dropout_probability = random.uniform(0, 1)
-            if not deterministic and (dropout_probability < self.layerdrop):  # skip the layer
-                layer_outputs = (None, None)
-            else:
-                layer_outputs = encoder_layer(
+            layer_outputs = encoder_layer(
                     hidden_states,
                     attention_mask,
                     output_attentions,
@@ -582,19 +578,15 @@ class FlaxWhisperDecoderLayerCollection(nn.Module):
             if output_hidden_states:
                 all_hidden_states += (hidden_states,)
                 # add LayerDrop (see https://arxiv.org/abs/1909.11556 for description)
-            dropout_probability = random.uniform(0, 1)
-            if not deterministic and (dropout_probability < self.layerdrop):
-                layer_outputs = (None, None, None)
-            else:
-                layer_outputs = decoder_layer(
-                    hidden_states,
-                    attention_mask,
-                    encoder_hidden_states,
-                    encoder_attention_mask,
-                    init_cache,
-                    output_attentions,
-                    deterministic,
-                )
+            layer_outputs = decoder_layer(
+                hidden_states,
+                attention_mask,
+                encoder_hidden_states,
+                encoder_attention_mask,
+                init_cache,
+                output_attentions,
+                deterministic,
+            )
 
             hidden_states = layer_outputs[0]
             if output_attentions:
